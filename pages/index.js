b072b422bffb44ed1aca47113fc56533dbd4373a
Tim Tse <timtse76@gmail.com>
2023-02-16 19:29:43 -0500

    made modal boostrap based

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 
-import {
-  Container,
-  Grid,
-  Button
-} from '@mui/material';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 import NewItemModal from 'components/elements/modals/NewItemModal';
 
@@ -12,11 +11,11 @@ export default function Home() {
   const [isNewItemModalOpen, setIsNewItemModalOpen] = useState(false);
   return (
     <Container>
-      <Grid>
-        <Grid item>
-          <Button onClick={() => setIsNewItemModalOpen(true)}>Open</Button>
-        </Grid>
-      </Grid>
+      <Row>
+        <Col>
+        <Button onClick={() => setIsNewItemModalOpen(true)}>Open</Button>
+        </Col>
+      </Row>
       <NewItemModal open={isNewItemModalOpen} onClose={() => setIsNewItemModalOpen(false)} />
     </Container>
   );
--- src/components/elements/modals/NewItemModal/index.js
+++ src/components/elements/modals/NewItemModal/index.js
@@ -1,37 +1,14 @@
-import { Modal, Box, TextField, Grid, Button } from "@mui/material";
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 500,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 2,
-  p: 4,
-};
-
+import Modal from 'react-bootstrap/Modal';
 
 export default function NewItemModal({ open, onClose }) {
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-    >
-      <Box sx={style}>
-        <Grid>
-          <Grid item>
-            <TextField fullWidth id="programName" label="Name" />
-          </Grid>
-          <Grid item>
-            <TextField fullWidth id="programMemberId" label="Member ID" />
-          </Grid>
-          <Grid item>
-            <Button variant="contained">Submit</Button>
-          </Grid>
-        </Grid>
-      </Box>
+    <Modal show={open} onHide={onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add New Item</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Test
+      </Modal.Body>
     </Modal>
   );
 }
